a61f364bef19f9008e6af14b5085633f414f8f3c
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-02-25 16:15:51 +0530

    Add files via upload

---
+++ basic/practice07.java
@@ -0,0 +1,12 @@
+package basic;
+
+//escape sequence
+
+
+class escSequnce {
+	public static void main(String[] args)
+	{
+    System.out.println("Hi , welcome to \"Java Practice Repository\".");
+    System.out.println("Good Afternoon\t Everyone! ");
+	}
+}
