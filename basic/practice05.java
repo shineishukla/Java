e275d80cef55ecd4525298e06f5d722c0ad8b9bb
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-02-25 15:31:17 +0530

    Add files via upload

---
+++ basic/practice05.java
@@ -0,0 +1,18 @@
+package basic;
+
+//output methods
+
+//System.out.print() and System.out.println()
+class Testmenthod{
+    public static void main(String[] args) {
+        int x=10, y=20;
+        char z='a';
+        String str="Shinei";
+        System.out.println(x);
+        System.out.print(z);
+        System.out.println(x+y);
+        System.out.println(x+" "+y);
+        System.out.print(str + " ");
+        System.out.print("Shukla");        
+    }
+}
