6510fb6db49b73e144fa22a4626de8ac6f8bbc32
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-03-02 21:23:13 +0530

    Add files via upload

---
+++ basic/practice25.java
@@ -0,0 +1,12 @@
+//while loop
+
+class practice25 {
+    public static void main (String [] args){
+        int i=6;
+        while(i>=10){
+            System.out.println("Hello");
+            i++;
+            //i=i+2; 
+        }
+    }
+}
