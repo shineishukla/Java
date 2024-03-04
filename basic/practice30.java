0d191ef4e17527469eff11fb2959ff15c1de4512
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-03-04 15:17:53 +0530

    Update practice30.java

--- basic/practice30.java
+++ basic/practice30.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 class practice30 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count=0;
-        while (n>0) {
-            n=n/10;
-            count=count+1;
-        } System.out.print(count);
+        try (Scanner scn = new Scanner(System.in)) {
+            int n = scn.nextInt();
+            int count=0;
+            while (n>0) {
+                n=n/10;
+                count=count+1;
+            } System.out.print(count);
+        }
     }
 }
