1a15df334548a764b49961768500db7621532eb6
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-03-04 14:35:14 +0530

    Add files via upload

---
+++ basic/practice29.java
@@ -0,0 +1,39 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+//continue and break
+
+/*class practice29
+{ 
+    public static void main(String[] args)throws IOException 
+    { 
+      for(int i=0; i<5; i++)
+      {
+          if(i==3)
+          {
+              continue;
+          }
+          System.out.println(i);
+      }
+    } 
+} */
+
+/*
+class practice29
+{ 
+    public static void main(String[] args)throws IOException 
+    { 
+      for(int i=0; i<5; i++)
+      {
+          if(i==3)
+          {
+              break;
+          }
+          
+          System.out.println(i);
+      }
+
+    } 
+} 
+*/
