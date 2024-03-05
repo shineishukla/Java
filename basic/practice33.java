b24e19cf6771001e000180106f6425ef2654ed1e
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-03-05 15:30:00 +0530

    Add files via upload

---
+++ basic/practice33.java
@@ -0,0 +1,17 @@
+//print pattern of * 5x5 
+
+class practice33{
+    public static void main(String[] args) {
+        int n=5;
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=n){
+                System.out.print("*");
+                j++;
+            }
+        System.out.println();    
+        i++;    
+        }
+    }
+}
