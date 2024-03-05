c5f3bfbd5b5aa0c3a7c9e2f55162dba85c230c74
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-03-05 15:10:45 +0530

    Add files via upload

---
+++ basic/practice32.java
@@ -0,0 +1,20 @@
+//pattern questions
+
+//print 5 * in one column
+class practice32{
+    public static void main(String[] args) {
+        /*int i=1;
+        while(i<=5){
+            System.out.println("*");
+            i++;
+        }*/
+        
+        //print 5 * in one row
+        int j=1;
+        while(j<=5){
+            System.out.print("*" + " ");
+            j++;
+        }
+
+    }
+}
