864a8d7e4badf02fa0149bbe9132d3474f9e7cbf
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2024-03-06 18:33:02 +0530

    Add files via upload

---
+++ basic/practice36.java
@@ -0,0 +1,21 @@
+//square pattern
+
+import java.util.Scanner;
+
+class practice36 {
+    public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            int n=scn.nextInt();
+            int i=1;
+            while(i<=n){
+                int j=1;
+                while(j<=n){
+                    System.out.print("*");
+                    j++;
+                }
+                i++;
+                System.out.println();
+            }
+        }
+    }
+}
